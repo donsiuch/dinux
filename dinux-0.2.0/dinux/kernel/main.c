3f02c14d4a54820a0ce416ecede864fd1ea17870
Donald Siuchninski <dsiuchninski@gmail.com>
2017-12-19 20:07:33 -0600

    Various changes to git, linker etc.

--- dinux-0.2.0/dinux/kernel/main.c
+++ dinux-0.2.0/dinux/kernel/main.c
@@ -4,13 +4,12 @@
 #include "x86/inc/mm.h"
 
 void kernel_main() {
-	/* Initialize terminal interface */
 	
 	terminal_initialize();
 	
 	printd("In the main loop\n");
 
-	//getFreeFrame();
+	getFreeFrame();
 
 	while (1) 
 	{
