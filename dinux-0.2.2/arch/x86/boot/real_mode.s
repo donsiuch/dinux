6e810d62a2aebeb5f6ad41894a6bcef3ef9a19a3
donnie <dsiuchninski@gmail.com>
2017-12-20 20:23:57 -0600

    Reformatted real mode code

--- dinux-0.2.2/arch/x86/boot/real_mode.s
+++ dinux-0.2.2/arch/x86/boot/real_mode.s
@@ -1,7 +1,5 @@
 
 .section .data.realmode
-real_data:
-    .word 0xABBA
 
 # IDT for real mode
 idt_real_info:
@@ -12,18 +10,16 @@ idt_real_info:
 
 .code32
 .section .text.realmode
+
 .globl switch_to_real_mode
 switch_to_real_mode:
-    pushl   %ebp
-    movl    %esp, %ebp
+    
+	pushl   %ebp
+    	movl    %esp, %ebp
 
-    # *********************
-	# Return to real mode *
-	# *********************
-
-    # Load a new segment with a limit of 0xffff 
-    # This is the segment limit required in real-
-    # address mode.
+    	# Load a new segment with a limit of 0xffff 
+    	# This is the segment limit required in real-
+    	# address mode.
 	jmp	$0x20, $loadRMSeg
 
 .code16
@@ -35,22 +31,14 @@ loadRMSeg:
 	movl	%eax, %es
 	movl	%eax, %ss
 
-    lidt	idt_real_info
-	#movw $0x03ff, (0x600)
-    #movw $0x00, (0x602)
-    #movw $0x00, (0x604)
-	#lidt 0x600
+    	lidt	idt_real_info
 
 	# Preserve %cr0
 	movl	%cr0, %eax
 
-	# Turn off PE bit, bit 0 to return to real mode
-	#
-	# want to do the following line but can't compile in 16 bit code
-    #andl	$0xffffffffe, %eax
-
-    # This is temporary
-    andl    $0xfffe, %eax
+    	# This is temporary
+	#andl	$0xffffffffe, %eax
+    	andl    $0xfffe, %eax
 	movl	%eax, %cr0
 
 	jmp	$0x00, $set_rm_segment_regs
@@ -63,43 +51,30 @@ set_rm_segment_regs:
 	movw	%ax, %ss
 
 	# Enable interrupts for memory checking.
-#	sti
+	sti
 
-    movb    $0x0e, %ah
-    movb    $'a', %al
-    int     $0x10
+    	movb    $0x0e, %ah
+    	movb    $'a', %al
+    	int     $0x10
 
-
-    # Working
 meme820:
 	xorl	%ebx, %ebx
 	#movw 	$smapBuffer, %di
-    movw    $0x9000, %di
-    # Pick some address that I anticipate to work
-    #movw    $0x500, %es:(%di)
+    	movw    $0x9000, %di
 	movl	$0x0000e820, %eax
 	movl 	$0x534D4150, %edx
 	movl	$20, %ecx
-	int	    $0x15
+	int	$0x15
 bail820:
 
-#   <code>
-    movw    $0xdead, %cx
+	cli
 
-
-#	cli
-
-	# **************************
-	# Return to protected mode *
-	# **************************
-
+	# Return to protected mode
 	xorw	%ax, %ax
 	movw	$0x0001, %ax
 	lmsw	%ax
 
 	jmp	$0x10, $restoreGDT
-# This directive is required to go here.
-
 .code32
 restoreGDT:
 	movl	$0x18, %eax
@@ -109,7 +84,8 @@ restoreGDT:
 	movl	%eax, %es
 	movl	%eax, %ss
 
-    movl    %ebp, %esp
-    popl    %ebp
-    ret
+    	movl    %ebp, %esp
+    	popl    %ebp
+    
+	ret
 
